2d47bb6c370c42cfeac91b91bf6c1e93847a13ea
Albert <albert.ramstedt@gmail.com>
2014-11-17 17:08:20 +0100

    make sure it handles incompatible input without throwing errors

--- lib/sie/document/renderer.rb
+++ lib/sie/document/renderer.rb
@@ -2,10 +2,11 @@ require "stringio"
 
 class Sie::Document::Renderer
   EMPTY_ARRAY = :empty_array
+  ENCODING = Encoding::CP437
 
   def initialize
     @io = StringIO.new
-    @io.set_encoding(Encoding::CP437)
+    @io.set_encoding(ENCODING)
   end
 
   def add_line(label, *values)
@@ -28,11 +29,15 @@ class Sie::Document::Renderer
   private
 
   def append(text)
-    io.puts(text)
+    io.puts(encoded(text))
   end
 
   def format_values(values)
     values.map { |value| format_value(value) }
+  end
+
+  def encoded(text)
+    text.encode(ENCODING, :invalid => :replace, :undef => :replace, :replace => "?")
   end
 
   def format_value(value)
