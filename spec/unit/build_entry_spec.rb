493f0a438f85054a6b1437eb3394d00a1eff130e
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-03-24 17:51:30 +0100

    Cleanup

--- spec/unit/build_entry_spec.rb
+++ spec/unit/build_entry_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "sie/parser/build_entry"
 require "sie/parser/tokenizer"
 
-describe Sie::Parser::BuildEntry, "call" do
+describe Sie::Parser::BuildEntry, ".call" do
   context "with an unexpected token at start of array" do
     it "raises InvalidEntryError" do
       line = '#TRANS 2400 [] -200 20130101 "Foocorp expense"'
